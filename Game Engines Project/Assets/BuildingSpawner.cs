8f15ecc33fcf7ba31104371cef8c342352e9d31d
Patnjop <36369847+Patnjop@users.noreply.github.com>
2018-12-17 03:43:50 +0000

    Range implemented

--- Game Engines Project/Assets/BuildingSpawner.cs
+++ Game Engines Project/Assets/BuildingSpawner.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class BuildingSpawner : MonoBehaviour
 {
-    Vector3 initialPos, targetPos;
+    Vector3 targetPos;
     Vector2 targetAdd;
     public GameObject buildingPrefab;
-    float maxTime = 1f;
-    public float maxRange;
+    float maxTime = 0.1f;
+    public int maxRange;
     float timer;
     int count, initialX, initialZ;
 
@@ -29,21 +29,20 @@ public class BuildingSpawner : MonoBehaviour
 
     void CreateBuilding()
     {
-        initialX = Random.Range(-4, 4);
-        initialZ = Random.Range(-4, 4);
-        //initialPos = new Vector3(initialX, 0f, initialZ);
+        initialX = Random.Range(-maxRange, maxRange + 1);
+        initialZ = Random.Range(-maxRange, maxRange + 1);
         targetPos = new Vector3(Mathf.RoundToInt(this.transform.position.x + initialX), 0.03f, Mathf.RoundToInt(this.transform.position.z + initialZ));
         targetAdd = new Vector2(targetPos.x, targetPos.z);
-        targetPos.y = 0.03f;
+        targetPos.y = 0.25f;
        
-        if (ListChecker.Values.Contains(targetPos))
-        {
-            
-        }
-        else
+        if (!ListChecker.Values.Contains(targetAdd))
         {
             ListChecker.Values.Add(targetAdd);
             GameObject newBuilding = Instantiate(buildingPrefab, targetPos, Quaternion.identity);
+        }
+        else
+        {
+            
         }
     }
 }
